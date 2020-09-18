a7de303f005dfd2c17568ec26b24098370ec57cd
NebukinaVA <sadpool@mail.ru>
2020-09-18 22:49:50 +0300

    main was put into .cpp, Remove was fixed

--- Zoo/Zoo/pch.h
+++ Zoo/Zoo/pch.h
@@ -18,165 +18,78 @@ public:
 		name = n;
 	}
 	virtual void Say() = 0;
-	int ReturnId()
-	{
-		return id;
-	}
-	~Animal()
-	{
-	}
+	int ReturnId();
+	~Animal() {}
 };
-
-int Animal::id_ = 0;
 
 class Mammal : public Animal // класс млекопитающие
 {
 public:
-	Mammal(std::string n) : Animal(n)
-	{
-	}
-	~Mammal()
-	{
-	}
+	Mammal(std::string n) : Animal(n) {}
+	~Mammal() {}
 };
 
 class Bird : public Animal // клкасс птицы
 {
 public:
-	Bird(std::string n) : Animal(n)
-	{
-	}
-	~Bird()
-	{
-	}
+	Bird(std::string n) : Animal(n) {}
+	~Bird() {}
 };
 
 class Cat : public Mammal // класс коты
 {
 	std::string sound = { "Meow!" };
 public:
-	Cat(std::string n = { "Who am I?" }) : Mammal(n)
-	{
-	}
-	void Say()
-	{
-		std::cout << sound << std::endl;
-	}
-	~Cat()
-	{
-	}
+	Cat(std::string n = { "Who am I?" }) : Mammal(n) {}
+	void Say();
+	~Cat() {}
 };
 
 class Dog : public Mammal // класс собаки
 {
 	std::string sound = { "Woof!" };
 public:
-	Dog(std::string n = { "Who am I?" }) : Mammal(n)
-	{
-	}
-	void Say()
-	{
-		std::cout << sound << std::endl;
-	}
-	~Dog()
-	{
-	}
+	Dog(std::string n = { "Who am I?" }) : Mammal(n) {}
+	void Say();
+	~Dog() {}
 };
 
 class Fox : public Mammal // класс лиса
 {
 	std::string sound = { "?????" };
 public:
-	Fox(std::string n = { "Who am I?" }) : Mammal(n)
-	{
-	}
-	void Say()
-	{
-		std::cout << sound << std::endl;
-	}
-	~Fox()
-	{
-	}
+	Fox(std::string n = { "Who am I?" }) : Mammal(n) {}
+	void Say();
+	~Fox() {}
 };
 
 class Sparrow : public Bird // класс воробей
 {
 	std::string sound = { "Tweet-tweet" };
 public:
-	Sparrow(std::string n = { "Who am I?" }) : Bird(n)
-	{
-	}
-	void Say()
-	{
-		std::cout << sound << std::endl;
-	}
-	~Sparrow()
-	{
-	}
+	Sparrow(std::string n = { "Who am I?" }) : Bird(n) {}
+	void Say();
+	~Sparrow() {}
 };
 
 class Cockerel : public Bird // класс петух
 {
 	std::string sound = { "Cock-A-Doodle-Do!" };
 public:
-	Cockerel(std::string n = { "Who am I?" }) : Bird(n)
-	{
-	}
-	void Say()
-	{
-		std::cout << sound << std::endl;
-	}
-	~Cockerel()
-	{
-	}
+	Cockerel(std::string n = { "Who am I?" }) : Bird(n) {}
+	void Say();
+	~Cockerel() {}
 };
 
 class Zoo // класс зоопарк
 {
 	std::vector <Animal*> animals;
 public:
-	Zoo()
-	{
-		Fox fox;
-		animals.push_back(&fox);
-	}
-	void Append(Animal* a)
-	{
-		animals.push_back(a);
-	}
-	void Remove(int ident) //id
-	{
-		std::vector <Animal*> temp(animals);
-		if (animals.empty()) throw "There are no animals to remove :(";
-		for (int i = 0;i < animals.size(); i++)
-		{
-			if (animals[i]->ReturnId() == ident)
-			{
-				temp.erase(animals.begin() + i);
-				animals.resize(animals.size() - 1);
-				animals.swap(temp);
-			}
-		}
-	}
-	void Listen() 
-	{
-		for (int i = 0; i < animals.size();i++)
-			animals[i]->Say();
-	}
-	~Zoo()
-	{
-	}
+	Zoo();
+	void Append(Animal* a);
+	void Remove(int ident); //id
+	void Listen();
+	~Zoo() {}
 };
 
-int main()
-{
-	Cat cat("Sprinkles");
-	cat.Say();
-	Zoo zoo;
-	zoo.Append(&cat);
-	zoo.Listen();
-	zoo.Remove(2);
-	zoo.Remove(1);
-	zoo.Remove(0);
-}
-#endif //PCH_H
+#endif
